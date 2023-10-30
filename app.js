4d8f05669f54183ae15302e25480c989a1f79619
mack2807 <vinaynvs9@gmail.com>
2023-10-31 00:27:37 +0530

    app.js

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ dotenv.config({ path: './config.env' })
 const bookRouter = require('./routes/bookRoutes');
 require('./controllers/db')
 
-app.use('/api/v1', bookRouter)
+app.use('/api', bookRouter)
 const port = process.env.PORT || 3009
 app.listen(port, () => {
     console.log(`App running on port ${port}...`)
